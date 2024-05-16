95f846654312690eef56601eb208e63de336fdee
Dioclei <jasonepic0801@gmail.com>
2024-05-16 19:40:22 +0800

    create file creation shortcut

---
+++ create_file.py
@@ -0,0 +1,15 @@
+leetcode_name = input("Enter Name of Problem: ")
+file_type = input("Enter Type [options: java / python / pandas]: ").lower()
+actual_file_name = leetcode_name.replace(".", "").replace(" ", "_").lower()
+
+if file_type == "java":
+    actual_file_name = "Java/" + actual_file_name + ".py"
+elif file_type == "python":
+    actual_file_name = "python/" + actual_file_name + ".py"
+elif file_type == "pandas":
+    actual_file_name = "python/30_days_of_pandas/" + actual_file_name + ".py"
+else:
+    raise ValueError("Invalid type of file!")
+
+with open(actual_file_name, "x") as f:
+    print("Created file at " + actual_file_name)
