b2c9f2af89b4879580c5666bc8296f773a47019f
Dioclei <jasonepic0801@gmail.com>
2024-05-27 01:16:47 +0800

    update create_file.py

--- create_file.py
+++ create_file.py
@@ -1,7 +1,7 @@
 leetcode_name = input("Enter Name of Problem: ")
-file_type = input("Enter Type [options: java / python / pandas]: ").lower()
 actual_file_name = leetcode_name.replace(".", "").replace(" ", "_").lower()
 
+file_type = "python"    # input("Enter Type [options: java / python / pandas]: ").lower()
 if file_type == "java":
     actual_file_name = "Java/" + actual_file_name + ".py"
 elif file_type == "python":
