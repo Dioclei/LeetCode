e1018fff2a2701f6b4bc438c4a80fcc842268461
Dioclei <jasonepic0801@gmail.com>
2024-06-10 08:55:50 +0800

    1051

---
+++ python/1051_height_checker.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = heights.copy()
+        expected.sort()
+        mismatches = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                mismatches += 1
+        return mismatches
